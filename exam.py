ddef0c63e556d6522ea2d96bfa17573fee3bb25f
AntonRudko <120172522+AntonRudko@users.noreply.github.com>
2022-12-09 11:58:17 +0200

    Add files via upload

---
+++ exam.py
@@ -0,0 +1,29 @@
+with open("text.txt", "r") as f:
+    data = f.readlines()
+arr =[]
+k = 0
+h = 0
+for i1, i in enumerate(data):
+    if i == '\n':
+        arr.append([])
+        for j in range(h, i1):
+            arr[k].append(data[j])
+        h = i1
+        k += 1
+
+
+a = input("Enter your word -> ")
+m = []
+for i1, i in enumerate(arr):
+    for j in i:
+        if a in j:
+            m.append(i1)
+            break
+
+
+if len(m) != 0:
+    for j in m:
+        for i in arr[j]:
+            print(i)
+else:
+    print("Error")
